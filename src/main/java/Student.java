6d46bf143bea7bcbef9ba091342d73cfc0057a0b
lizhaoming <zhaoming.li@aliyu.com>
2018-05-11 16:35:39 +0800

    添加了Student类

---
+++ src/main/java/Student.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author zhaom
+ *
+ */
+public class Student {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
